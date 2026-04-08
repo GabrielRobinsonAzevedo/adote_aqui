9aba3464d264caeecd5c3486662f081207d0e0a4
Gian-Moreira <1gianmoreira@gmail.com>
2026-04-07 21:31:00 -0300

    Add files via upload
    Front end do programa Adote Aqui

---
+++ src/frontEnd/verMais.php
@@ -0,0 +1,41 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Nome do PET</h1>
+        <hr>
+    </header>
+
+    <main id="informacoesDoPet">
+        <article id="dadosDoPet">
+            <img src="assets/dogstock.jpg" alt="Pet">
+            <div id="informacoesGerais">
+                <p id="sexo"><strong>Sexo:</strong>Macho</p>
+                <p id="especie"><strong>Espécie:</strong>Cachorro</p>
+                <p id="dataDeNascimento"><strong>Data de nascimento:</strong>09/09/2024</p>
+                <p id="porte"><strong>Porte:</strong>Grande</p>
+                <p id="pesoAtual"><strong>Peso atual:</strong>14,10kg</p>
+                <p id="corDaPelagem"><strong>Cor da pelagem:</strong>Preta</p>
+                <p id="raca"><strong>Raça:</strong>Beagle</p>
+                <p id="vacinado"><strong>Vacinado:</strong>Sim</p>
+                <p id="castrado"><strong>Castrado:</strong>Nao</p>
+            </div>
+            
+            <div id="descricao">
+                <h2>Descricao:</h2>
+                <p id="descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+
+            <a href="index.php"><button class="botao corPrincipal">Voltar</button></a>
+            <a href=""><button class="botao corSecundaria">Entrar em contato</button></a>
+
+        </article>
+
+    </main>
+</body>
+</html>
